792a02f0b7ba252e9eacd1baece474385715f50c
FelipeAC04 <felipeemailcurso123@gmail.com>
2024-11-27 16:42:14 -0300

    Add files via upload

---
+++ Personagem.ts
@@ -0,0 +1,9 @@
+export interface Personagem {
+    nome: string
+    vida: number
+    forca: number
+    defesa: number
+
+    atacar(inimigo: Personagem): void
+    perderVida(dano: number): void
+}
---
+++ teste.ts
@@ -0,0 +1,11 @@
+getNome(): string
+setNome(nome: string): string
+
+getVida(): number
+setVida(vida: number): number
+
+getForca(): number
+setForca(forca: number): number
+
+getDefesa(): number
+setDefesa(defesa: number): number
